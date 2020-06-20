66932357edf8edf3998296935f0a34cef75d7505
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-06-20 17:21:18 +0530

    Update - check desc
    Remove lightslider
    Add slick
    Add photos in gallery
    Remove _slides

---
+++ _testimonials/Addison Limited.markdown
@@ -0,0 +1,11 @@
+---
+title: 'Addison Limited'
+image: ''
+businessurl: ''
+name: 'Giridhara Gopal,'
+jobtitle: 'Wholetime Director'
+date: 2016-01-15
+---
+
+
+> Balaji Rajaraman has provided us with ISO 9001:2015 documentation and training support and his services were outstanding. We have no hesitation in recommending his services for companies wishing to engage him for consulting services in management systems implementation and certification. I wish him all the best in his business endeavours.
--- js/main.js
+++ js/main.js
@@ -8,3 +8,47 @@ $(function () {
     $(this).select();
 	});
 });
+
+$(document).ready(function(){
+  $('.gallery-carousel').slick({
+      lazyLoad: 'ondemand',
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+              }
+          },
+          {
+              breakpoint: 600,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+      ]
+  });
+  $('.testimonials-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 7000,
+  });
+});
+
