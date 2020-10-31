9b7b35e4380352db9db948f715fba2925c106c48
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-10-31 21:16:56 +0530

    update client logos

--- js/main.js
+++ js/main.js
@@ -1,54 +1,85 @@
 
 $(function () {
-	$('a.embed').on('click', function () {
-		$(this.parentNode).children('.embed-pane').toggle();
-		return false;
-	});
-	$('.embed-input').on("click", function () {
-    $(this).select();
-	});
+    $('a.embed').on('click', function () {
+        $(this.parentNode).children('.embed-pane').toggle();
+        return false;
+    });
+    $('.embed-input').on("click", function () {
+        $(this).select();
+    });
 });
 
-$(document).ready(function(){
-  $('.gallery-carousel').slick({
-      lazyLoad: 'ondemand',
-      infinite: true,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      responsive: [
-          {
-              breakpoint: 1024,
-              settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 1,
-              }
-          },
-          {
-              breakpoint: 600,
-              settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1
-              }
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-              }
-          }
-      ]
-  });
-  $('.testimonials-carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 7000,
-  });
+$(document).ready(function () {
+    $('.gallery-carousel').slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.testimonials-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 7000,
+    });
+    $('.client_logo-carousel').slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
 
